d854e046df79ecb122876a239277ddc59b451c44
kuro <thanhchi.fit.hcmus@gmail.com>
2026-08-07 12:03:21 +0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int[] pre : prerequisites) {
+            graph.computeIfAbsent(pre[0], k -> new ArrayList<>()).add(pre[1]);
+        }
+
+        // -1 for false, 0 is not compute yet and 1 is computed
+        int[] state = new int[numCourses];
+        boolean[] visiting = new boolean[numCourses];
+        for (int[] pre : prerequisites) {
+            if (!dfs(pre[0], graph, visiting, state)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean dfs(
+        int course,
+        Map<Integer, List<Integer>> graph,
+        boolean[] visiting, 
+        int[] state
+    ) {
+        if (state[course] != 0) {
+            return state[course] == 1;
+        }
+        // Cycle detected
+        if (visiting[course]) {
+            return false;
+        }
+
+        visiting[course] = true;
+        for (int pre : graph.computeIfAbsent(course, k -> List.of())) {
+            if (!dfs(pre, graph, visiting, state)) {
+                state[course] = -1;
+                return false;
+            }
+        }
+
+        visiting[course] = false;
+        state[course] = 1;
+        return true;
+    }
+}
